11595668a4b0fe797e67e4a92e9b9c2f34bc4dd9
Mike Heath <elcapo@gmail.com>
2014-05-27 12:50:54 -0600

    Added LICENSE. Fixed JavaDoc parsing error.

--- src/main/java/netty/syslog/Message.java
+++ src/main/java/netty/syslog/Message.java
@@ -1,3 +1,19 @@
+/*
+ *   Copyright (c) 2014 Intellectual Reserve, Inc.  All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 package netty.syslog;
 
 import io.netty.buffer.ByteBuf;
@@ -13,7 +29,7 @@ import java.util.Map;
 /**
  * Represents a Syslog content as defined by RFC 5424. See http://tools.ietf.org/html/rfc5424#section-6.
  *
- * @author Mike Heath <elcapo@gmail.com>
+ * @author "Mike Heath <elcapo@gmail.com>"
  */
 public class Message extends DefaultByteBufHolder {
 
